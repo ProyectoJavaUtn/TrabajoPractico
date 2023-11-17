8aab5fb53f341962467b633c7726e21b5f72f8c7
Nicolás <nicolascesarini94@hotmail.com>
2023-11-17 19:53:19 -0300

    Agregamos las clases ya hechas anteriormente al nuevo proyecto

---
+++ src/main/java/models/Tecnico.java
@@ -0,0 +1,29 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Tecnico {
+    static int generadorID = 0;
+    private int IDTecnico;
+    // private ArrayList<Especialidad> especialidadesTecnico;
+    private ArrayList<Incidente> incidentesTecnico;
+
+    public Tecnico (){
+        this.IDTecnico = generadorID++;
+        this.incidentesTecnico = new ArrayList<Incidente>();
+    }
+
+    public void agregarIncidente (Incidente i){
+        this.incidentesTecnico.add(i);
+    }
+    public void quitarIncidente (Incidente i){
+        this.incidentesTecnico.remove(i);
+    }
+    public ArrayList<Incidente> getincidentesTecnico(){
+        return incidentesTecnico;
+    }
+    //Getters y Setters
+    public int getIDTecnico() {
+        return IDTecnico;
+    }
+}
